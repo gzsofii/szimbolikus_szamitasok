4430302d3347fa61b844936a4e6fc08067e7ceb0
Bundas102 <robert.ban102@gmail.com>
2018-07-01 15:49:19 +0200

    pow szabály javítás

--- pow_rules.py
+++ pow_rules.py
@@ -1,34 +1,36 @@
 from simplify import *
+from rules import add_rule
 
 pow_rules = []
-
+"""
 # 1^x = 1
-pow_1_a = Rule(F('^', 1, Var('x', 'complex'))),
-				1,
-				'pow')
-pow_rules.append(pow_1_a)
+add_rule(pow_rules,
+	F('^', 1, Var('x', 'complex')),
+	1,
+	'pow', 'oneway')
 # x^1 = x
-pow_z_1 = Rule(F('^', Var('x', 'complex'), 1)),
-				Var('x', 'complex'),
-				'pow')
-pow_rules.append(pow_z_1)
+add_rule(pow_rules,
+	F('^', Var('x', 'complex'), 1),
+	Var('x', 'complex'),
+	'pow', 'oneway')
 # x^0 = 1 ### x != 0
-pow_z_0 = Rule(F('^', Var('x', 'complex_nonzero'), 0)),
-				1,
-				'pow')
-pow_rules.append(pow_z_0)
+add_rule(pow_rules,
+	F('^', Var('x', 'complex_nonzero'), 0),
+	1,
+	'pow', 'oneway')
 # x^a * x^b = x^(a+b)
-pow_add = Rule(AC0('*', F('^', Var('x', 'complex'), Var('a', 'complex')), F('^', Var('x', 'complex'), Var('b', 'complex'))),
-				F('^', Var('x', 'complex'), AC0('+', Var('a', 'complex'), Var('b', 'complex'))),
-				'pow')
-pow_rules.append(pow_add)
+add_rule(pow_rules,
+	AC0('*', F('^', Var('x', 'complex'), Var('a', 'complex')), F('^', Var('x', 'complex'), Var('b', 'complex'))),
+	F('^', Var('x', 'complex'), AC0('+', Var('a', 'complex'), Var('b', 'complex'))),
+	'pow')
+"""
 # (xy)^a = x^a * y^a
-pow_mul = Rule(F('^', AC0('*', Var('x', 'complex'), Var('y', 'complex')), Var('a', 'complex')),
-				AC0('*', F('^', Var('x', 'complex'), Var('a', 'complex')), F('^', Var('y', 'complex'), Var('a', 'complex'))),
-				'pow')
-pow_rules.append(pow_mul)
+add_rule(pow_rules,
+	F('^', AC0('*', Var('x', 'complex'), Var('y', 'complex')), Var('a', 'complex')),
+	AC0('*', F('^', Var('x', 'complex'), Var('a', 'complex')), F('^', Var('y', 'complex'), Var('a', 'complex'))),
+	'pow')
 # (x^a)^b = x^(ab)
-pow_pow = Rule(F('^', Var('x', 'complex'), F('^', Var('a', 'complex'), Var('b', 'complex'))),
-				F('^', Var('x', 'complex'), AC0('*', Var('a', 'complex'), Var('b', 'complex'))),
-				'pow')
-pow_rules.append(pow_pow)
+add_rule(pow_rules,
+	F('^', F('^', Var('x', 'complex'), Var('a', 'complex')), Var('b', 'complex')),
+	F('^', Var('x', 'complex'), AC0('*', Var('a', 'complex'), Var('b', 'complex'))),
+	'pow')
